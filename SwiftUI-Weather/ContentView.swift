2a26a435ae193519dab3a769e7ffd2fd0a11e304
Frantisek Varadzin <varadzin@gmail.com>
2021-05-13 16:10:13 +0200

    update

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
                            startPoint: .topLeading,
                             endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
             VStack {
-                Text("Kosice")
+                Text("Hannover")
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
@@ -30,6 +30,15 @@ struct ContentView: View {
                     Text("35°")
                         .font(.system(size: 70, weight: .medium))
                         .foregroundColor(.white)
+                }
+                .padding(.bottom, 40)
+                
+                HStack(spacing: 20) {
+                    WeatherDayView(dayOfWeek: "MON", imageName: "cloud.sun.fill", temperature: 34)
+                    WeatherDayView(dayOfWeek: "TUE", imageName: "sun.max.fill", temperature: 38)
+                    WeatherDayView(dayOfWeek: "WED", imageName: "wind.snow", temperature: 25)
+                    WeatherDayView(dayOfWeek: "THU", imageName: "sunset.fill", temperature: 30)
+                    WeatherDayView(dayOfWeek: "FRI", imageName: "snow", temperature: 4)
                 }
                 Spacer()
                 
@@ -46,5 +55,30 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            
+        }
     }
 }
