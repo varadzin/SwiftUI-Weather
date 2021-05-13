85842ce8d937e2eac1c6e94007e08775ba365df9
Frantisek Varadzin <varadzin@gmail.com>
2021-05-13 13:36:55 +0200

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Frantisek Varadzin on 13/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
